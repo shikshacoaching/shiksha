3332155e751f1d1d1c387e4c8122195755e166b0
shikshacoaching <shikshacentre779@gmail.com>
2026-03-25 20:35:34 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,33 @@
 const CACHE_NAME = 'shiksha-v1';
 const ASSETS = [
-  'index.html',
-  'style.css',
-  'manifest.json',
+  './',
+  './index.html',
+  './style.css',
+  './manifest.json',
   'https://iili.io/qkieRf9.png'
 ];
 
+// Install Event - Caches local assets
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching assets');
+      return cache.addAll(ASSETS);
+    })
   );
+  self.skipWaiting(); // Forces the waiting service worker to become the active service worker
 });
 
+// Activate Event
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim()); // Allows the service worker to take control of the page immediately
+});
+
+// Fetch Event - Network first, then Cache
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    fetch(e.request).catch(() => {
+      return caches.match(e.request);
+    })
   );
 });
