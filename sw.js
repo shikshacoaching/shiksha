f50e9ea9664aef3b4b834c065f9c4ca81ccb2163
shikshacoaching <shikshacentre779@gmail.com>
2026-03-26 17:14:56 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,10 @@
-const CACHE_NAME = 'shiksha-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/assets/css/style.css',
-  '/assets/js/script.js',
-  'https://cdn.tailwindcss.com'
-];
+const cacheName = 'shiksha-v2';
+const files = ['./', './index.html', './assets/css/style.css', './assets/js/script.js'];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(files)));
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
